f2e0824326f14860f934cb51e71e4bdeff766a33
wolfslayer <diegodisante22@gmail.com>
2025-04-17 21:41:54 +0200

    Update main(copy).py

--- drone_codes/code_copy/main(copy).py
+++ drone_codes/code_copy/main(copy).py
@@ -1,37 +1,40 @@
 from drone_functions import *
 
 if __name__ == '__main__':
-    on_of = False
-    connectTello("F09956")
+    connectTello("F09947")
     drone.connect()
-    if on_of:
-        drone.streamon()
-    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     speed = 50
-    control = [0, 0, 0, 0]
     i = 0
 
     while not is_pressed("esc"):
+        control = [0, 0, 0, 0]
+        print(control)
+        drone.streamon()
         if is_pressed("z") and not drone.is_flying: drone.takeoff()
-        if is_pressed("x") and drone.is_flying: drone.land()
-        if is_pressed("e"): drone.set_video_direction(drone.CAMERA_DOWNWARD)
-        if is_pressed("q"): drone.set_video_direction(drone.CAMERA_FORWARD)
+        elif is_pressed("x") and drone.is_flying: drone.land()
+        camera()
+
         if is_pressed("+")and speed < 100:
-            sleep(0.01)
-            speed = max(100, speed +1)
-        if is_pressed("-") and speed > 0:
-            sleep(0.01)
-            speed = min(0, speed - 1)
-
-        control = isPressed(speed)
+            sleep(0.001)
+            speed += 1
+        elif is_pressed("-") and speed > 0:
+            sleep(0.001)
+            speed -= 1
+        if is_pressed("a"): control[0] = -speed
+        elif is_pressed("d"): control[0] = speed
+        if is_pressed("s"): control[1] = -speed
+        elif is_pressed("w"): control[1] = speed
+        if is_pressed("k"): control[2] = -speed
+        elif is_pressed("i"): control[2] = speed
+        if is_pressed("j"): control[3] = -speed
+        elif is_pressed("l"): control[3] = speed
+        
         drone.send_rc_control(control[0], control[1], control[2], control[3])
-        if on_of:
-            img = show_img(speed)
-
+        img = show_img(speed)
         if is_pressed("space"):
             cv2.imwrite(f"file{i}.jpg", img)
-            sleep(0.1)
             i += 1
+            sleep(0.5)
 
         cv2.imshow("Output", img)
         cv2.waitKey(1)
